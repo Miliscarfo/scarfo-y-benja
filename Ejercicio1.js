1cca967a47f7c0871ee88449b3d21ac85853a8d5
Miliscarfo <mi.scarfo@colegiocalasanz.edu.ar>
2025-09-18 15:07:10 -0300

    Add files via upload

---
+++ Ejercicio1.js
@@ -0,0 +1,6 @@
+alert("hola mundo");
+let boton = document.getElementById("boton");
+let parrafo = document.getElementById("texto");
+boton.addEventListener("click", function(){
+    parrafo.innerHTML="Marynellis";
+});
